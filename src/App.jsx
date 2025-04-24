a69ca2c08f518c8cb0e9c59000ec1503f18e451b
rhv94 <rebeca_hv@hotmail.com>
2025-04-24 09:32:07 -0300

    login-page

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,19 @@ export default function App(){
             <div className="w-full h-[15%] flex items-center justify-center">
               <h1 className="text-[30px] text-[#00ff88] font-bold">LOGIN</h1>
             </div>
-            <div className="w-full h-[70%] p-[20px]">
+            <div className="w-full h-[60%] p-[20px]">
               <label htmlFor="user" className="text-white">Usuário</label>
               <input type="text" id="user" className="w-full rounded-md p-[10px] bg-[#514768] text-white mb-4" />
 
               <label htmlFor="pass" className="text-white">Senha</label>
               <input type="password" id="pass" className="w-full rounded-md p-[10px] bg-[#514768] text-white"/>
+              <div className="w-full flex items-center justify-center text-white pt-1">
+                <a href="">Esqueceu sua senha?</a>
+              </div>
             </div>
-            <div className="w-full h-[15%] "></div>
+            <div className="w-full h-[25%] p-[20px] flex items-center">
+              <button className="w-full h-[50px] text-[18px] font-bold text-[#201b2c] rounded-lg bg-[#00ff88] shadow-lg shadow-[#33f79c63]">LOGIN</button>
+            </div>
           </div>
         </div>
       </div>
